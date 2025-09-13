eadef167f6fc0428193feccb7608638984d95959
Erick Segura <103792031+Erick0096@users.noreply.github.com>
2025-09-13 13:00:15 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-<h1> Sistema de Registro </h1>
+Este es un pequeño proyecto JavaScript que permite jugar al clásico "Amigo Secreto"  
 
-Estado de proyecto en construccion
+Como funciona:
+1. Abre el archivo "index.html" en tu navegador.  
+2. Escribe un nombre en el campo de texto.  
+3. Haz clic en "Añadir" para que el nombre aparezca en la lista.  
+4. Repite el proceso para añadir todos los amigos que quieras.  
+5. Haz clic en "Sortear amigo" y aparecerá un nombre al azar.
--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Ejecutado el sistema")
+let amigos = [];
+
+function agregarAmigo() {
+  let input = document.getElementById("amigo");
+  let nombre = input.value;
+
+  if (nombre === "") {
+    alert("Escribe un nombre válido.");
+    return;
+  }
+
+  amigos.push(nombre);
+  input.value = "";
+
+  document.getElementById("listaAmigos").innerHTML = amigos.map(a => `<li>${a}</li>`).join("");
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Agrega al menos un nombre.");
+    return;
+  }
+
+  let indice = Math.floor(Math.random() * amigos.length);
+  document.getElementById("resultado").innerHTML = `<li>Tu amigo secreto es: ${amigos[indice]}</li>`;
+}
